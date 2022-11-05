c1e6fbf133f47c35bac4776b636335e0c953bbb6
nizya-mirai <106097784+nizya-mirai@users.noreply.github.com>
2022-11-06 00:00:34 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ function btn_click(word){
     wacca.innerHTML = ("afterend", `<div id="wacca">${word}</div>`);
 }
 
-//ボタンがクリックされたら発火
+//"生成する"ボタンがクリックされたら発火
 document.getElementById("btn_check").onclick = function buttonClick() {
     let random_check = document.getElementById("ran_check");
     let CSSstop = document.getElementById("img-wacca");
@@ -22,10 +22,11 @@ document.getElementById("btn_check").onclick = function buttonClick() {
     if (random_check.checked == true){
         //構文呼び出し用のランダム数
         let ran_num = Math.floor(Math.random() * words.length);
+        let ran2_num = Math.floor(Math.random() * words2.length);
         //構文ランダム用のランダム数
         let shift_num = Math.floor(Math.random() * emotions.length);
         //生成された数字を元に別jsファイルの配列呼び出し?
-        let ran_word = `${words[ran_num]}の${words2[ran_num]}${emotions[shift_num]}すぎだろ！`;
+        let ran_word = `${words[ran_num]}の${words2[ran2_num]}${emotions[shift_num]}すぎだろ！`;
         //ティーダのチ*ポが気持ちよかったら処理に進む
         if(ran_word == "ティーダのチ*ポ気持ちよすぎだろ！"){
             //ボタンを不活性化
@@ -34,11 +35,12 @@ document.getElementById("btn_check").onclick = function buttonClick() {
             btn_click(ran_word);
             //3秒経つとボタンが活性化
             setTimeout(function() {btn_dis.disabled = false;}, 3000);
-            setTimeout(function() {CSSstop.classList.remove("wacca", "up-dn");}, 3000);
+            setTimeout(function() {CSSstop.classList.remove("wacca");}, 3000);
         //気持ち良くなかったらこっち
         }else{
             btn_dis.disabled = true;
-            btn_click(ran_word);
+            let fix_ran_word = ran_word.replace("気持ちよくないすぎだろ！", "気持ちよくない")
+            btn_click(fix_ran_word);
             setTimeout(function() {btn_dis.disabled = false;}, 100);
         }
     //チェックボックスの状態がtrue以外だったら処理に進む
